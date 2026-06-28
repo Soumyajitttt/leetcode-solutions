a1b433d87efce7cc3e02f008c1708769a73502f8
Soumyajit Chaudhury <soumyajitchaudhury4@gmail.com>
2026-06-28 14:35:57 +0530

    Add solution: Add Two Numbers (Medium)

--- Medium/2_Add_Two_Numbers.java
+++ Medium/2_Add_Two_Numbers.java
@@ -39,15 +39,40 @@
  * ----- Solution -----
  */
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int k=1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]!=nums[i-1]){
-                nums[k]=nums[i];
-                k++;
-            }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode tail = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int digit1 = (l1 != null) ? l1.val : 0;
+            int digit2 = (l2 != null) ? l2.val : 0;
+
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            ListNode newNode = new ListNode(digit);
+            tail.next = newNode;
+            tail = tail.next;
+
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
         }
-        return k;
+
+        ListNode result = dummyHead.next;
+        dummyHead.next = null;
+        return result;
     }
 }
