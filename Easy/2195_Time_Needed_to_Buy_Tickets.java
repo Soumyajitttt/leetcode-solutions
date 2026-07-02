ce5a9f0f2763a265487b2bd0a69cc4cfed49b402
Soumyajit Chaudhury <soumyajitchaudhury4@gmail.com>
2026-07-02 23:10:48 +0530

    Add solution: Time Needed to Buy Tickets (Easy)

--- Easy/2195_Time_Needed_to_Buy_Tickets.java
+++ Easy/2195_Time_Needed_to_Buy_Tickets.java
@@ -64,9 +64,10 @@
 
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
+        Queue <Integer> q = new LinkedList<>();
         int time=0;
-        Queue<Integer>q= new LinkedList<>();
-        for(int i=0;i<tickets.length;i++){
+
+        for(int i=0; i< tickets.length; i++){
             q.add(i);
         }
 
@@ -76,15 +77,15 @@ class Solution {
 
             tickets[front]--;
 
-            if(tickets[front]!=0){
+            if(tickets[front] != 0 ){
                 q.add(front);
-            }
-            else{
-                if(front==k){
+            }else{
+                if(front == k){
                     return time;
                 }
             }
         }
-        return time;     
+
+        return time;
     }
 }
