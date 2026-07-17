61a2feccda835ac575e8777abb01401e10ccd613
Soumyajit Chaudhury <soumyajitchaudhury4@gmail.com>
2026-07-17 22:46:10 +0530

    Add solution: Power of Two (Easy)

--- Easy/231_Power_of_Two.java
+++ Easy/231_Power_of_Two.java
@@ -44,7 +44,7 @@
 
 class Solution {
     public boolean isPowerOfTwo(int n) {
-       if(n==0) return false;
+        if(n==0) return false;
         if(n<0) return false;
         while(n!=1){
             if(n%2==1)return false;
