8f42e33241b4771265e482801342dd4355a16acc
Soumyajit Chaudhury <soumyajitchaudhury4@gmail.com>
2026-07-20 00:57:45 +0530

    Add solution: Missing Number (Easy)

--- Easy/268_Missing_Number.java
+++ Easy/268_Missing_Number.java
@@ -79,14 +79,11 @@
 
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-
-        if(nums[0] != 0) return 0;
-
-        for(int i = 1; i < nums.length; i++){
-            if(nums [i] != nums[i-1] + 1) return nums[i-1] + 1;
+        int n = nums.length;
+        int sum = n*(n+1)/2;
+        int newsum= 0;
+        for(int i = 0;i<nums.length;i++){
+            newsum+=nums[i];
         }
-
-        return nums.length;
-    }
+    return sum-newsum;}
 }
