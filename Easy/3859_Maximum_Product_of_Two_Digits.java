fd97d11d2e3921ef76ccad5f329a81ea434026d6
Soumyajit Chaudhury <soumyajitchaudhury4@gmail.com>
2026-08-06 22:08:09 +0530

    Add solution: Maximum Product of Two Digits (Easy)

--- Easy/3859_Maximum_Product_of_Two_Digits.java
+++ Easy/3859_Maximum_Product_of_Two_Digits.java
@@ -74,13 +74,14 @@ class Solution {
 
         while(n>0){
             int rem = n%10;
-            if(max1<=rem){
+            if(max1 <= rem){
                 max2 = max1;
                 max1 = rem;
-            }else if(max2<=rem) max2 = rem;
+            }else if (max2 <= rem) max2 = rem;
+
             n/=10;
         }
 
-        return max1*max2;
+        return max1 * max2;
     }
 }
