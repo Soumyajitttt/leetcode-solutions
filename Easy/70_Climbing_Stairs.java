5194a2eb77268a05ee195f83731a9d4472df70df
Soumyajit Chaudhury <soumyajitchaudhury4@gmail.com>
2026-08-09 00:43:05 +0530

    Add solution: Climbing Stairs (Easy)

--- Easy/70_Climbing_Stairs.java
+++ Easy/70_Climbing_Stairs.java
@@ -39,16 +39,14 @@
 
 class Solution {
     public int climbStairs(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
+        if(n == 1 || n == 2) return n;
 
         int a = 1;
         int b = 2;
-
-        for (int i = 3; i <= n; i++) {
-            int c = a + b;
+        for(int i = 3; i <= n; i++){
+            int temp = a + b;
             a = b;
-            b = c;
+            b = temp ;
         }
 
         return b;
