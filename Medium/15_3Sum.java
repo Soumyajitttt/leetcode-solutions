ba0bdd5b01f95823228d6eeab6821891afdd33ae
Soumyajit Chaudhury <soumyajitchaudhury4@gmail.com>
2026-08-13 01:48:41 +0530

    Add solution: 3Sum (Medium)

--- Medium/15_3Sum.java
+++ Medium/15_3Sum.java
@@ -52,22 +52,23 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         int n = nums.length;
         for(int i = 0;i < n - 2;i++){
-        int j = i+1,k = n - 1;
+            int j = i+1,k = n - 1;
+            
+            if(i > 0 && nums[i] == nums[i-1]) continue;
         
-        if(i > 0 && nums[i] == nums[i-1]) continue;
-        
-        while(j < k){
-            int sum = nums[i] + nums[j] + nums[k];
-            if(sum == 0){
-            res.add(Arrays.asList(nums[i],nums[j],nums[k]));
-            j++;
-            k--;
-            while(j < k && nums[j] == nums[j-1]) j++;
-            }else if(sum < 0) j++;
-            else k--;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0){
+                res.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                j++;
+                k--;
+                while(j < k && nums[j] == nums[j-1]) j++;
+                }
+                else if(sum < 0) j++;
+                else k--;
 
+            }
         }
-       }
 
        return res;
 
