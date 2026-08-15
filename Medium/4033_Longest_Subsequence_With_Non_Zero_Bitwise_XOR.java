e65d6a8ffd729ede1113322aeeafc9706515ec99
Soumyajit Chaudhury <soumyajitchaudhury4@gmail.com>
2026-08-15 23:10:59 +0530

    Add solution: Longest Subsequence With Non-Zero Bitwise XOR (Medium)

---
+++ Medium/4033_Longest_Subsequence_With_Non_Zero_Bitwise_XOR.java
@@ -0,0 +1,69 @@
+/*
+ * Problem #4033: Longest Subsequence With Non-Zero Bitwise XOR
+ * Difficulty: Medium
+ *
+ * ----- Description -----
+ *
+ * You are given an integer array nums.
+ * 
+ * Return the length of the longest subsequence in nums whose bitwise XOR is non-zero. If no such subsequence exists, return 0.
+ * 
+ *  
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,2,3]
+ * 
+ * Output: 2
+ * 
+ * Explanation:
+ * 
+ * One longest subsequence is [2, 3]. The bitwise XOR is computed as 2 XOR 3 = 1, which is non-zero.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [2,3,4]
+ * 
+ * Output: 3
+ * 
+ * Explanation:
+ * 
+ * The longest subsequence is [2, 3, 4]. The bitwise XOR is computed as 2 XOR 3 XOR 4 = 5, which is non-zero.
+ * 
+ * 
+ *  
+ * Constraints:
+ * 
+ * 
+ * 	1 <= nums.length <= 105
+ * 	0 <= nums[i] <= 109
+ *
+ * ----- Solution -----
+ */
+
+class Solution {
+    public int longestSubsequence(int[] nums) {
+        int xor = 0;
+        boolean hasNonZero = false;
+
+        for (int num : nums) {
+            xor ^= num;
+
+            if (num != 0) {
+                hasNonZero = true;
+            }
+        }
+
+        if (xor != 0) {
+            return nums.length;
+        }
+
+        if (hasNonZero) {
+            return nums.length - 1;
+        }
+
+        return 0;
+    }
+}
